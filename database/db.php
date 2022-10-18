321c8f4be2b1f48a61bee7f221a5756810d08d48
Childew <childeric23@gmail.com>
2022-10-18 11:53:27 +0200

    try and catch sur la bdd

--- database/db.php
+++ database/db.php
@@ -1,14 +1,18 @@
 <?php
 
 const DB_HOST = 'db.3wa.io';
-const DB_PORT = '3306';
+const DB_PORT = '3304';
 const DB_NAME = 'williamduribreux_ProjetFramework';
 const DB_USERNAME = 'williamduribreux';
 const DB_PASSWORD = '503a2b6cb045bb165556ae9c32a46ae3';
 
 function getConnect():\PDO{
 
-    return new PDO(
-        'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD
-    );
+    try{
+        return new PDO(
+            'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD
+        );
+    } catch (PDOException $e){
+        echo 'erreur de connexion à la base de donnée <br>'.$e -> getMessage();
+    }
 }
